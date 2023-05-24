19e7cf4b812d46adea096a1a0735e36ab0ba9282
Silviya Slavcheva <silviya.slavcheva@gmail.com>
2023-05-24 22:30:01 +0300

    result folder

--- tests_api_perf_tests2.ps1
+++ tests_api_perf_tests2.ps1
@@ -1,5 +1,5 @@
 
-& "C:\Users\Sylvia\Documents\jmeter2\jmeterproject2\apache-jmeter-5.5\bin\jmeter.bat" -n -t "C:\Users\Sylvia\Documents\jmeter2\jmeterproject\apache-jmeter-5.5\bin\test2.jmx" -l "C:\Users\Sylvia\Downloads\buildAgentFull\work\b74038a8e1f1354f\Results\Logs.jtl"
+& "C:\Users\Sylvia\Documents\jmeter2\jmeterproject2\apache-jmeter-5.5\bin\jmeter.bat" -n -t "C:\Users\Sylvia\Documents\jmeter2\jmeterproject2\apache-jmeter-5.5\bin\test2.jmx" -l "C:\Users\Sylvia\Downloads\buildAgentFull\work\30c0cfd17454bb5d\Results\Logs.jtl"
 
 
 
